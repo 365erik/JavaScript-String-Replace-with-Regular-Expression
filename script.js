e16976d6e72e02560f1d27112813146b42032c62
Erik Smith <6515842+365erik@users.noreply.github.com>
2022-03-23 21:22:33 +0000

    Make the str.replaceAll implementation easier for beginners to understand.

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 const replaceFirstMatch = (str) =>
   str.replace(/{{(.*?)}}/, `<span style="color:green">$1</span>`);
 
+// Use /g flag in your regex to enable greedy matching
 const replaceAllMatches = (str) =>
   str.replace(/{{(.*?)}}/g, `<span style="color:blue">$1</span>`);
 
-const useStringPrototypeReplaceAll = (str) =>
-  // String.replaceAll() is not found in Nodejs until v15
-  typeof str.replaceAll === "function"
-    ? str.replaceAll(
-        new RegExp("{{(.*?)}}", "g"),
-        `<span style="color:red">$1</span>`
-      )
-    : `String.prototype.replaceAll() is supported in most browsers, 
+const useStringPrototypeReplaceAll = (str) => {
+  if (typeof str.replaceAll === "function") {
+    // Note replaceAll() requires explicit
+    // instantiation of a RegExp object.
+    const re = new RegExp("{{(.*?)}}", "g");
+    return str.replaceAll(re, `<span style="color:red">$1</span>`);
+  }
+
+  return `String.prototype.replaceAll() is supported in most browsers, 
 		but you won't find it in Nodejs until v15 and greater.`;
+};
 
 // Demo Setup
 const handleTextAreaInput = (str, el) => {
   el.innerHTML = "";
+
   const examples = [
     `<p>${replaceFirstMatch(str)}</p>`,
     `<p>${replaceAllMatches(str)}</p>`,
     `<p>${useStringPrototypeReplaceAll(str)}</p>`,
   ];
+
   examples.forEach((example) => el.insertAdjacentHTML("beforeend", example));
 };
 
